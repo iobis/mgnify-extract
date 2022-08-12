cf8e3bbc74a980cd2289ff3fedd53507460128dc
Pieter Provoost <pieterprovoost@gmail.com>
2022-08-12 16:44:10 +0200

    added test

--- test/test_studies.py
+++ test/test_studies.py
@@ -1,4 +1,6 @@
+from typing import List
 import unittest
+from mgnifyextract.samples import Sample
 from mgnifyextract.studies import Study, get_study
 
 
@@ -12,3 +14,11 @@ class TestStudies(unittest.TestCase):
         self.assertIsInstance(study, Study)
         self.assertIsNotNone(study.data)
         self.assertEquals(study.data["id"], STUDY_ID)
+
+    def test_get_samples(self):
+        study = get_study(STUDY_ID)
+        samples = study.get_samples(max_results=2)
+        self.assertIsInstance(samples, List)
+        self.assertEquals(len(samples), 2)
+        self.assertIsInstance(samples[0], Sample)
+        self.assertIsNotNone(samples[0].data)
